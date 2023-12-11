64c759f7511b14d8a9280563896f48912320d110
Kazuki Kubo <kz@assaulter.org>
2023-12-11 14:24:09 +0900

    chore: gitignore.

---
+++ GraphQLSample/GraphQLSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLSampleApp.swift
+//  GraphQLSample
+//
+//  Created by Kazuki Kubo on 2023/12/11.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
