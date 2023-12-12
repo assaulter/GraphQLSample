254e75da5c507ce3c89d7c34abf083b9d1e92a20
Kazuki Kubo <kz@assaulter.org>
2023-12-12 10:29:52 +0900

    完成

---
+++ Packages/API/Sources/API/Generated/Schema/Objects/Root.graphql.swift
@@ -0,0 +1,11 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension SW.Objects {
+  static let Root = ApolloAPI.Object(
+    typename: "Root",
+    implementedInterfaces: []
+  )
+}
